7e5b27dc9dd064498e347d1508adbf42f87896bc
Abishek <abimano99@gmail.com>
2018-07-20 18:32:28 +0530

    Initial commit

---
+++ js/creative.js
@@ -0,0 +1,40 @@
+(function($) {
+    "use strict"; // Start of use strict
+  
+     // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 57
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Scroll reveal calls
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+      duration: 1000,
+      delay: 200
+    });
+    sr.reveal('.sr-contact', {
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
+    }, 300);
+    
+  })(jQuery); // End of use strict
